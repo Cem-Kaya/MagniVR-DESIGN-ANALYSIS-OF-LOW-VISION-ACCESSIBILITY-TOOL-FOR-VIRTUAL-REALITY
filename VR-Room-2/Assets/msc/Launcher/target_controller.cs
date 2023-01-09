204c0544b15765262f89bdd61c57d32a0d729ea4
cem-kaya <cemkaya2704@gmail.com>
2023-01-09 19:00:39 +0300

    bug fix

--- VR-Room-2/Assets/msc/Launcher/target_controller.cs
+++ VR-Room-2/Assets/msc/Launcher/target_controller.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class target_controller : MonoBehaviour
 {
 	// Start is called before the first frame update
+	bool been_hit = false;
 	void Start()
 	{
-		
+		been_hit = false
 	}
+	
 	// function to write to the text file
 	public void write_to_file(string text)
 	{
@@ -25,7 +27,8 @@ public class target_controller : MonoBehaviour
 	public void OnCollisionEnter(Collision collision)
 	{
 		//Debug.Log("name"+collision.gameObject.name);
-		if (collision.gameObject.name == "Projectile_Dart(Clone)") {
+		if (collision.gameObject.name == "Projectile_Dart(Clone)" && !been_hit ) {
+			been_hit = true;
 			// get time 
 			string time = System.DateTime.Now.ToString("HH:mm:ss.fff");
 			write_to_file("hit on " + time + "\n");
