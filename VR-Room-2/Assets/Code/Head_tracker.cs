199d9544398715918ad2f7a133733dc785b36e72
BahaMert <bersoy@sabanciuniv.edu>
2023-03-25 19:41:44 +0300

    mag infront of cam

--- VR-Room-2/Assets/Code/Head_tracker.cs
+++ VR-Room-2/Assets/Code/Head_tracker.cs
@@ -6,42 +6,39 @@ using UnityEngine.XR;
 
 public class Head_tracker : MonoBehaviour
 {
-	// Start is called before the first frame update
-	// User needs to initialize XR Rig gameobject from scene explorer
-	[SerializeField] GameObject XRRig_object;
-	XROrigin XROrigin_script;
-	Camera XR_camera;
+	// Developer needs to assign XR_camera an controller from scene explorer
+	[SerializeField] private Camera XR_camera;
+	[SerializeField] private GameObject left_hand_controller;
+	[SerializeField] private GameObject right_hand_controller;
 
 
-	private void get_XROrigin_script()
-	{
-		XROrigin_script = XRRig_object.GetComponent<XROrigin>();
-	}
 
 
 	void Start()
 	{
-		get_XROrigin_script();
+	
 	}
 
-	public InputDevice hmd;
 	private void FixedUpdate()
 	{
 
+		Quaternion camera_rot = XR_camera.transform.rotation;
+        Vector3 camera_forward = XR_camera.transform.forward;
+        Vector3 camera_pos = XR_camera.transform.position;
+        //multipyle camera pos with rotation 
+		Debug.Log("cam pos: " + camera_pos);
+		
 
-		if(hmd!=null){
-			Vector3 device_pos;
-			Vector3 eye_pos;
-			Quaternion device_rot;
-			Quaternion eye_rot;
+		
+		Debug.Log("cam forward: " + camera_forward);			
+		Debug.Log("cam rot: " + camera_rot);
+		gameObject.transform.position = camera_pos + camera_forward * 0.3f;
+		gameObject.transform.rotation = camera_rot;
 
-			Debug.Log("device pos: " + device_pos);			
-			Debug.Log("eye pos: " + eye_pos);
-			gameObject.transform.position = device_pos;
-			gameObject.transform.rotation = device_rot;
-		} 
+        
 
-	}
+
+    }
 
 
 	// Update is called once per frame
