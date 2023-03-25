de68bd9ba024dd6a568fae0381e5e69f2cd80caa
cem-kaya <cemkaya2704@gmail.com>
2023-03-25 18:42:10 +0300

    juist xr api fail

--- VR-Room-2/Assets/Code/Head_tracker.cs
+++ VR-Room-2/Assets/Code/Head_tracker.cs
@@ -1,20 +1,55 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.XR;
 
 public class Head_tracker : MonoBehaviour
 {
-    // hello uwu woo wee its time to roll babyyy 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	// hello uwu woo wee its time to roll babyyy 
+	// Start is called before the first frame update
+	void Start()
+	{
+		
+	}
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public InputDevice hmd;
+	private void FixedUpdate()
+	{
+
+		var inputDevices = new List<InputDevice>();
+		InputDevices.GetDevices(inputDevices);
+
+		foreach (var device in inputDevices)
+		{
+			// Debug.Log(device.characteristics.ToString());
+			if (device.characteristics.ToString() == "HeadMounted, TrackedDevice") {
+				hmd  = device;
+			}
+
+		}
+		if(hmd!=null){
+			Vector3 device_pos;
+			Vector3 eye_pos;
+			Quaternion device_rot;
+			Quaternion eye_rot;
+			hmd.TryGetFeatureValue(CommonUsages.devicePosition, out device_pos);
+			hmd.TryGetFeatureValue(CommonUsages.centerEyePosition, out  eye_pos);
+			hmd.TryGetFeatureValue(CommonUsages.deviceRotation, out device_rot);
+			hmd.TryGetFeatureValue(CommonUsages.centerEyeRotation, out eye_rot);
+
+
+			Debug.Log("device pos: " + device_pos);			
+			Debug.Log("eye pos: " + eye_pos);
+			gameObject.transform.position = device_pos;
+			gameObject.transform.rotation = device_rot;
+		} 
+
+	}
+
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
 }
