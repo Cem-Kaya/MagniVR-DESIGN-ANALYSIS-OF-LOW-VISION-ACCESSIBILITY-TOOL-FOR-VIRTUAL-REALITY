79542aa65c69e7caf79d0332e6e5e0242f6c86d2
cem-kaya <cemkaya2704@gmail.com>
2023-03-25 17:06:29 +0300

    new scene

---
+++ VR-Room-2/Assets/Code/Head_tracker.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Head_tracker : MonoBehaviour
+{
+    // hello uwu woo wee its time to roll babyyy 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
