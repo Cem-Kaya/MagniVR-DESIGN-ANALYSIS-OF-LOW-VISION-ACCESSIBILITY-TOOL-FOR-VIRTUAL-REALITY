19c6b451909a2b398cf9cd01b018f4a3f2133d68
cem kaya <cemkaya2704@egmail.com>
2023-08-18 18:20:17 +0300

    jumping added for stage

--- VR-Room-2/Assets/Test_envo_assets/Target_shootign/Target_2_script.cs
+++ VR-Room-2/Assets/Test_envo_assets/Target_shootign/Target_2_script.cs
@@ -6,6 +6,11 @@ public class Target_2_script : MonoBehaviour
 {
     private bool been_hit = false;
     private float speed = 1;
+
+    private float jumpHeight = 0.10f; // Controls the height of the jump
+    private float jumpFrequency = 2.0f; // Controls how many jumps you want within the range
+    private float startingX;
+
     // Start is called before the first frame update
     private Target_2_system_script t1_sys;
     void Start()
@@ -14,20 +19,21 @@ public class Target_2_script : MonoBehaviour
         // multiply the speed by a random number inbetween 0.5 and 1.2
         speed *= UnityEngine.Random.Range(0.5f, 1.2f);
 
+        startingX = transform.localPosition.x;
+
     }
 
     // Update is called once per frame
     bool movingRight = true;
 
-    private void FixedUpdate() 
-    { 
-        //Debug.Log(transform.localPosition.x);
-
+    private void FixedUpdate()
+    {
         if (movingRight)
         {
             if (transform.localPosition.x < 5.0f)
             {
-                transform.localPosition += new Vector3(0.1f, 0, 0)* speed;
+                float jumpValue = Mathf.Sin((transform.localPosition.x - startingX) * jumpFrequency) * jumpHeight;
+                transform.localPosition += new Vector3(0.1f * speed, jumpValue, 0);
             }
             else
             {
@@ -38,7 +44,8 @@ public class Target_2_script : MonoBehaviour
         {
             if (transform.localPosition.x > -5.0f)
             {
-                transform.localPosition -= new Vector3(0.1f, 0, 0)* speed;
+                float jumpValue = Mathf.Sin((startingX - transform.localPosition.x) * jumpFrequency) * jumpHeight;
+                transform.localPosition -= new Vector3(0.1f * speed, jumpValue, 0);
             }
             else
             {
@@ -46,6 +53,8 @@ public class Target_2_script : MonoBehaviour
             }
         }
     }
+
+
     // on collusion 
     public void OnCollisionEnter(Collision collision)
     {
